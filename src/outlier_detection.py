959e239ef1ff15d3d8208cea38d9c4bbfc18a59c
Loki <105162957+lokeshbodas@users.noreply.github.com>
2025-06-25 22:26:31 +0530

    Update outlier_detection.py

--- src/outlier_detection.py
+++ src/outlier_detection.py
@@ -90,36 +90,4 @@ class OutlierDetector:
 
 # Example usage
 if __name__ == "__main__":
-    # # Example dataframe
-    # df = pd.read_csv("../extracted_data/AmesHousing.csv")
-    # df_numeric = df.select_dtypes(include=[np.number]).dropna()
-
-    # # Initialize the OutlierDetector with the Z-Score based Outlier Detection Strategy
-    # outlier_detector = OutlierDetector(ZScoreOutlierDetection(threshold=3))
-
-    # # Detect and handle outliers
-    # outliers = outlier_detector.detect_outliers(df_numeric)
-    # df_cleaned = outlier_detector.handle_outliers(df_numeric, method="remove")
-
-    # print(df_cleaned.shape)
-    # # Visualize outliers in specific features
-    # # outlier_detector.visualize_outliers(df_cleaned, features=["SalePrice", "Gr Liv Area"])
     pass
-
-
-#    SalePrice  Gr Liv Area
-# 0     200000        1500
-# 1     300000        2000
-# 2     400000        2500
-# 3     500000        3000
-# 4    1000000        4000
-# 5    2000000       10000
-
-
-#    SalePrice  Gr Liv Area
-# 0     200000        1500
-# 1     300000        2000
-# 2     400000        2500
-# 3     500000        3000
-# 4    1000000        4000
-# 5    1000000        4000
